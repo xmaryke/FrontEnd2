a8a2cf1dcf2cc66150695fb46236ddd24e6c2e91
xmaryke <marykedekker@gmail.com>
2013-11-24 19:41:43 +0100

    Swipe function + animation

--- ScoreApp/scoreApp.js
+++ ScoreApp/scoreApp.js
@@ -17,34 +17,95 @@ var SCOREAPP = SCOREAPP || {};
     SCOREAPP.schedule = null;
     SCOREAPP.ranking = null; 
 
+    //Gives user feedback
+    SCOREAPP.feedback = {
+        //Loader - feedback for loading data
+        showLoader: function(){
+            document.getElementById('floatingBarsG').style.display = "block";
+        },
+        hideLoader: function(){
+            document.getElementById('floatingBarsG').style.display = "none";
+        },
+
+        //See which page is active in menu
+        liRanking: qwery('nav li#liRanking')[0],
+        liSchedule: qwery('nav li#liSchedule')[0],
+        liRankingActive: function() {
+
+            this.liRanking.classList.remove('navInactive');
+            this.liRanking.classList.add('navActive');
+
+            this.liSchedule.classList.remove('navActive');
+            this.liSchedule.classList.add('navInactive');
+        },
+        liScheduleActive: function() {
+
+            this.liSchedule.classList.remove('navInactive');
+            this.liSchedule.classList.add('navActive');
+
+            this.liRanking.classList.remove('navActive');
+            this.liRanking.classList.add('navInactive');
+        }
+    };
+
+
     //Router (Routie library) 
     SCOREAPP.router = {
         init: function (){
             //loads HTML & data for each page
             routie({
                 '/game/:id': function(id) { //Takes the id out of the URL
-                    document.getElementById('floatingBarsG').style.display = "block"; //Shows loader
+                    SCOREAPP.feedback.showLoader(); //Shows loader
                     document.getElementById('navigation').style.display = "none"; //Hide navigation
                 	console.log("routie game");
                     SCOREAPP.datas.game(id); //Gets game data from API from specific game_id 
+                    SCOREAPP.animation.init(); //Start frisbee animation
                 },
                 '/schedule': function() {
-                    document.getElementById('floatingBarsG').style.display = "block"; //Shows loader
+                    SCOREAPP.feedback.liScheduleActive(); //Show nav item as active 
+                    SCOREAPP.feedback.showLoader(); //Shows loader
                 	console.log("routie schedule");
                     SCOREAPP.datas.schedule(); //Gets schedule data from API
                 },
                 '/ranking': function() {
-                    document.getElementById('floatingBarsG').style.display = "block"; //Shows loader
+                    SCOREAPP.feedback.liRankingActive(); //Show nav item as active
+                    SCOREAPP.feedback.showLoader(); //Shows loader
                 	console.log("routie ranking");
                     SCOREAPP.datas.ranking(); //Gets ranking data from API
                 },
                 //Default output for undefined URLs
                 '*': function() { //Displays schedule page
-                    document.getElementById('floatingBarsG').style.display = "block"; //Shows loader
+                    SCOREAPP.feedback.liScheduleActive(); //Show nav item as active 
+                    SCOREAPP.feedback.showLoader(); //Shows loader
                     console.log("routie default");
                     SCOREAPP.datas.schedule(); 
                 }
             });
+        },
+
+        //Control number for the swipe function
+        controlNumber: 0,
+
+        //Prevents to execute the swipe function multiple times
+        swipe: function() {
+            //After executing the swipe function add 1 to the controlNumber
+            this.controlNumber += 1;
+
+            //When the swipe function is executed 2 times, the control number will be 2 and then the code below will be executed for one time
+            if(this.controlNumber == 2) {
+                if(window.location.hash == "#/schedule"){ //If you're on the schedule page go to the ranking page 
+                     window.location.href = "http://marrymedia.nl/frontend/#/ranking";
+                }
+                else if(window.location.hash == "#/ranking"){ //If you're on the ranking page go to the schedule page
+                    window.location.href = "http://marrymedia.nl/frontend/#/schedule";
+                }
+                else{ //If you're on another page go to the ranking page
+                    window.location.href = "http://marrymedia.nl/frontend/#/ranking";
+                }
+
+                //Reset the controle number to value 0
+                this.controlNumber -= 2;
+            }
         },
 
         //Switch to another page
@@ -105,7 +166,7 @@ var SCOREAPP = SCOREAPP || {};
                 game_id: gameId, 
                 team_1_score: team1Score.value,
                 team_2_score: team2Score.value,
-                is_final: isFinal.value
+                is_final: "True"
             }
 
             //Acces to post function
@@ -125,6 +186,7 @@ var SCOREAPP = SCOREAPP || {};
                     window.location.href = "http://marrymedia.nl/frontend/#/schedule";
                     //Reload schedule page
                     window.location.reload(true);
+
 
                 } else {
                     console.warn(error);
@@ -149,7 +211,7 @@ var SCOREAPP = SCOREAPP || {};
                    if (!err) {
                         SCOREAPP.game = result; //Fill SCOREAPP.game with data
                         Transparency.render(qwery('[data-route=game]')[0], SCOREAPP.game); //Render data to HTML section with data-route="game" 
-                        document.getElementById('floatingBarsG').style.display = "none"; //Hide loader
+                        SCOREAPP.feedback.hideLoader(); //Hides loader
                         SCOREAPP.router.gameSection(); //Change to game page
 
                         //Form function for edit score
@@ -159,7 +221,7 @@ var SCOREAPP = SCOREAPP || {};
                 });
             }else{
                 Transparency.render(qwery('[data-route=game]')[0], SCOREAPP.game);
-                document.getElementById('floatingBarsG').style.display = "none";
+                SCOREAPP.feedback.hideLoader(); 
                 SCOREAPP.router.gameSection();
             }
         },
@@ -171,14 +233,14 @@ var SCOREAPP = SCOREAPP || {};
                 site.get(function (err, result) {
                    if (!err) {
                         SCOREAPP.schedule = result.objects; //Fill SCOREAPP.schedule with data objects
-                        Transparency.render(qwery('[data-table=schedule]')[0], SCOREAPP.schedule, directives); //Render data & directives to HTML section with data-table="schedule"
-                        document.getElementById('floatingBarsG').style.display = "none"; //Hide loader
+                        Transparency.render(qwery('[data-list=schedule]')[0], SCOREAPP.schedule, directives); //Render data & directives to HTML section with data-table="schedule"
+                        SCOREAPP.feedback.hideLoader(); //Hides loader
                         SCOREAPP.router.change(); //Change to schedule page
                     }
                 });
             }else {
-                Transparency.render(qwery('[data-table=schedule]')[0], SCOREAPP.schedule, directives);
-                document.getElementById('floatingBarsG').style.display = "none";
+                Transparency.render(qwery('[data-list=schedule]')[0], SCOREAPP.schedule, directives);
+                SCOREAPP.feedback.hideLoader(); //Hides loader
                 SCOREAPP.router.change();
             }  
         },
@@ -191,13 +253,13 @@ var SCOREAPP = SCOREAPP || {};
                    if (!err) {
                         SCOREAPP.ranking = result; //Fill SCOREAPP.ranking with data 
                         Transparency.render(qwery('[data-route=ranking]')[0], SCOREAPP.ranking); //Render data to HTML section with data-route="ranking"
-                        document.getElementById('floatingBarsG').style.display = "none"; //Hide loader
+                        SCOREAPP.feedback.hideLoader(); //Hides loader
                         SCOREAPP.router.change(); //Change to ranking page
                    } 
                 });
             }else{
                 Transparency.render(qwery('[data-route=ranking]')[0], SCOREAPP.ranking);
-                document.getElementById('floatingBarsG').style.display = "none";
+                SCOREAPP.feedback.hideLoader(); //Hides loader
                 SCOREAPP.router.change();
             } 
         }
@@ -209,6 +271,16 @@ var SCOREAPP = SCOREAPP || {};
                 return params.text = "#/game/" + this.id;
             }
         },
+        teams: {
+            text: function(params) {
+            return this.team_1.name + " VS " + this.team_2.name;
+            }
+        },
+        scores: {
+            text: function(params) {
+            return this.team_1_score + " - " + this.team_2_score;
+            }
+        },
         start_time: { //directive to display only time
             text: function(params) {
                 var dateTime = this.start_time;
@@ -217,6 +289,37 @@ var SCOREAPP = SCOREAPP || {};
             }
         }
     };
+
+
+    //Animations in the Score App
+    SCOREAPP.animation = { 
+
+        init: function(){ //Starts the animation function doMove
+            var frisbee = document.getElementById('frisbee');
+            frisbee.style.left = '0%'; //sets begin position 
+            SCOREAPP.animation.doMove();
+        },
+
+        doMove: function(){ //Moves frisbee to the right
+            frisbee.style.left = parseInt(frisbee.style.left)+1+'%'; 
+            setTimeout(SCOREAPP.animation.doMove,20); //Activates function doMove every 20msec
+            if(frisbee.style.left == "100%"){ //When the fribee reachs the end of the page go to the very left of the page
+                frisbee.style.left = parseInt(frisbee.style.left)-100+'%';
+            }
+        }
+    }; 
+
+
+    //When the user swiped right change page
+    $$('article#content').swipeRight(function() {
+        SCOREAPP.router.swipe();
+    });
+
+    //When the user swiped left change page
+    $$('article#content').swipeLeft(function() {
+        SCOREAPP.router.swipe();
+    });
+           
 
     //DOM ready
     domready(function () {
